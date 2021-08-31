700753075d26c9ac27588f0a04aae3d0db96b7b4
bzangi <bruno_zangi@hotmail.com>
2021-08-30 23:47:43 -0300

    criada classe Pessoa

--- src/main/kotlin/one/digitalinnovation/dioBank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/dioBank/Pessoa.kt
@@ -3,11 +3,12 @@ package one.digitalinnovation.dioBank
 class Pessoa {
     var nome: String = "Brunao"
     var cpf: String = "123.123.123-12"
+    private set
 }
 
 fun main() {
     val pessoa = Pessoa()
 
     println(pessoa.nome)
-    print(pessoa.cpf)
+    println(pessoa.cpf)
 }
